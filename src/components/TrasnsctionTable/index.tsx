919e70c9601ea50f9cbb518e7b8787956d95ffd3
Felipe Leal <felipewanderson2020@gmail.com>
2021-03-15 16:11:27 -0300

    :lipstick: compoent TrasnsctionTable

---
+++ src/components/TrasnsctionTable/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from "./styles";
+
+export function TransactionTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Preço</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de sites</td>
+            <td className="deposit">R$ 12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Alugel</td>
+            <td className="withdrawal">-R$ 500,00</td>
+            <td>Casa</td>
+            <td>10/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
