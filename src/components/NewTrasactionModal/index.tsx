0e3aa3d1669b1fca770d2746a5cd80c2c583900e
Felipe Leal <felipewanderson2020@gmail.com>
2021-03-16 11:40:52 -0300

    :lipstick: estilização do modal newTransactionModal

--- src/components/NewTrasactionModal/index.tsx
+++ src/components/NewTrasactionModal/index.tsx
@@ -1,4 +1,6 @@
 import ReactModal from "react-modal";
+import closeImg from '../../assets/close.svg';
+import { Container } from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,8 +12,25 @@ export function NewTransactionModal({ isOpen, onRequestClose }:NewTransactionMod
     <ReactModal 
         isOpen={isOpen}
         onRequestClose={onRequestClose}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
       >
-        <h2>Cadastrar nova transação</h2>
+        <Container>
+          <button 
+            type="button" 
+            className="react-modal-close" 
+            onClick={onRequestClose}
+          >
+            <img src={closeImg} alt="Fechar Modal" />  
+          </button>  
+          <h2>Cadastrar nova transação</h2>
+          <input placeholder="Título"/>
+          <input type="number" placeholder="Valor"/>
+          <input placeholder="Categoria"/>
+          <button type="submit">
+            Cadastrar
+          </button>
+        </Container>
       </ReactModal>
   )
 }
