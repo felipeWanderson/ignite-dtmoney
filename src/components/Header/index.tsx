e206356e88eb7503c47a57bfb4924705413b3d05
Felipe Leal <felipewanderson2020@gmail.com>
2021-03-16 10:30:48 -0300

    :construction: Modal Trasnsctions
    configutando a modal de transaction

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,33 @@
+import { useState } from 'react';
+import Modal from 'react-modal'
 import LogoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
+
 export function Header() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+    setIsNewTransactionModalOpen(false);
+  }
   return (
     <Container>
       <Content>
         <img src={LogoImg} alt="dt money"/>
-        <button type="button">
+        <button type="button" onClick={handleOpenNewTransactionModal}>
           Nova transação
         </button>
       </Content>
+
+      <Modal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar nova transação</h2>
+      </Modal>
     </Container>
   );
 }
