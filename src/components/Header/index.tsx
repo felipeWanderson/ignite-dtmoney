50c75682f67edb6c4c693b8d5ed835081848fdb2
Felipe Leal <felipewanderson2020@gmail.com>
2021-03-16 10:47:04 -0300

    :construction: New Trasaction Modal
    refatorando a modal pra se aplicar a toda a aplicação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,33 +1,21 @@
-import { useState } from 'react';
-import Modal from 'react-modal'
 import LogoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export function Header() {
-  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
-
-  function handleOpenNewTransactionModal() {
-    setIsNewTransactionModalOpen(true);
-  }
-
-  function handleCloseNewTransactionModal(){
-    setIsNewTransactionModalOpen(false);
-  }
+interface HeaderProps {
+  onOpenNewTrasactionModal: () => void;
+}
+export function Header({onOpenNewTrasactionModal}: HeaderProps) {
+  
   return (
     <Container>
       <Content>
         <img src={LogoImg} alt="dt money"/>
-        <button type="button" onClick={handleOpenNewTransactionModal}>
+        <button type="button" onClick={onOpenNewTrasactionModal}>
           Nova transação
         </button>
       </Content>
 
-      <Modal 
-        isOpen={isNewTransactionModalOpen}
-        onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar nova transação</h2>
-      </Modal>
+      
     </Container>
   );
 }
